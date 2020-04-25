72bfbebc26d9cee41c37c019f52645a420a8fe77
Adrian Osmond <a.c.osmond@gmail.com>
2020-04-25 16:43:28 +0200

    Moved cities to fit better

--- src/components/City/City.js
+++ src/components/City/City.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import classes from './City.module.css';
 
-const City = ({ name, zIndex, transform, opacity }) => {
+const City = ({ name, zIndex, transform, opacity, color, infections }) => {
+  const infectionRows = Object.entries(infections)
+    .filter(([col, number]) => col === color || number > 0)
+    .sort((a, b) => {
+      if (a === color) return -1;
+      if (b === color) return 1;
+      return a < b ? -1 : 1;
+    });
   return (
     <div className={classes.city} style={{ zIndex, transform, opacity }}>
       {name}
+      {infectionRows.map(([col, count]) => (
+        <div className={classes.infectionRow}>
+          <div
+            className={[
+              classes.pill,
+              classes[col],
+              count > 0 ? classes.filled : '',
+            ].join(' ')}
+          ></div>
+          <div
+            className={[
+              classes.pill,
+              classes[col],
+              count > 1 ? classes.filled : '',
+            ].join(' ')}
+          ></div>
+          <div
+            className={[
+              classes.pill,
+              classes[col],
+              count > 2 ? classes.filled : '',
+            ].join(' ')}
+          ></div>
+        </div>
+      ))}
     </div>
   );
 };
